9dbe4c4a9d8b2b75c4d7ae25c528cc6fe0a2ca3c
crazyconv <conanconv@163.com>
2015-10-26 21:56:22 +0800

    recursive files

--- sentences.py
+++ sentences.py
@@ -1,9 +1,10 @@
-import pandas;
-import nltk.data;
+import pandas
+import nltk.data
 from nltk.corpus import stopwords
 from util import CsvOption, ProcessOption
 import util
 import os
+import fnmatch
 
 
 class Sentences(object):
@@ -12,18 +13,23 @@ class Sentences(object):
 		self.csv_option = csv_option
 		self.process_option = process_option
 
+	def paragraph_iterator(self):
+		for root, dirs, files in os.walk(self.dir_name):
+			for file_name in fnmatch.filter(files, "*.csv"):
+				paragraphs = pandas.read_csv(os.path.join(root, file_name), \
+					delimiter=self.csv_option.deli, names=self.csv_option.title, \
+					iterator=True, chunksize=self.csv_option.chunksize)
+				for chunk in paragraphs:
+					# split review into sentences using NLTK tokenizer
+					for paragraph in chunk[self.csv_option.review_name]:
+						yield paragraph
+
 	def __iter__(self):
-		for file_name in os.listdir(self.dir_name):
-			paragraphs = pandas.read_csv(os.path.join(self.dir_name, file_name), \
-				delimiter=self.csv_option.deli, names=self.csv_option.title, \
-				iterator=True, chunksize=self.csv_option.chunksize);
-			tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-			stop_words = set(stopwords.words("english"));
-			for chunk in paragraphs:
-				# split review into sentences using NLTK tokenizer
-				for paragraph in chunk[self.csv_option.review_name]:
-					raw_sentences = tokenizer.tokenize(paragraph.decode('utf8').strip())
-					for raw_sentence in raw_sentences:
-						# process the sentence
-						sentence = util.process(raw_sentence, self.process_option, stop_words);
-						yield sentence
+		tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+		stop_words = set(stopwords.words("english"))
+		for paragraph in self.paragraph_iterator():
+			raw_sentences = tokenizer.tokenize(paragraph.decode('utf8').strip())
+			for raw_sentence in raw_sentences:
+				# process the sentence
+				sentence = util.process(raw_sentence, self.process_option, stop_words)
+				yield sentence
